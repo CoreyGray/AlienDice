96ad923218f5e66cda524e29e24b2d55d98a9107
Corey Gray <coreygray1@gmail.com>
2013-01-03 06:52:18 -0500

    Added project

---
+++ AlienDiceAPI/AlienDiceAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AlienDiceAPI.WebApiApplication" Language="C#" %>
---
+++ AlienDiceAPI/AlienDiceAPI/Models/AlienDiceGameState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlienDiceAPI.Models
+{
+    public class AlienDiceGameState
+    {
+        private System.Guid gID;
+	    public System.Guid ID
+	    {
+		    get { return gID;}
+		    set { gID = value;}
+	    }
+        private int intPlayersTurn;
+        public int PlayersTurn
+        {
+            get { return intPlayersTurn; }
+            set { intPlayersTurn = value; }
+        }
+        
+    }
+}
